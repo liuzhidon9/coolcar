d159d55c8424e6d58e6e6200524aaa225f11aca7
liuzhidong <909297022@qq.com>
2021-04-26 23:09:16 +0800

    实现地图首页

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -4,6 +4,47 @@ const app = getApp<IAppOption>()
 
 Page({
   data: {
+    setting : {
+      skew: 0,
+      rotate: 0,
+      showLocation: true,
+      showScale: true,
+      subKey: '',
+      layerStyle: 1,
+      enableZoom: true,
+      enableScroll: true,
+      enableRotate: false,
+      showCompass: false,
+      enable3D: false,
+      enableOverlooking: false,
+      enableSatellite: false,
+      enableTraffic: false,
+    },
+   location:{
+    longitude:120,
+    latitude:31,
+   },
+   scale:15,
+   is3D:false,
+   isOverLooking:true,
+    markers:[
+      {
+        id:0,
+        longitude:113,
+        latitude:23,
+        iconPath:"/resource/car/blue.png",
+        width:30,
+        height:30,
+      },
+      {
+        id:1,
+        longitude:114,
+        latitude:24,
+        iconPath:"/resource/car/yellow.png",
+        width:30,
+        height:30,
+      }
+    ],
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
@@ -15,6 +56,25 @@ Page({
   bindViewTap() {
     wx.navigateTo({
       url: '../logs/logs',
+    })
+  },
+  onMyLocationTap(){
+    wx.getLocation({
+      type:"gcj02",
+      success:res=>{
+        this.setData({
+          location:{
+            longitude:res.longitude,
+            latitude:res.latitude
+          }
+        })
+      },
+      fail:()=>{
+        wx.showToast({
+          icon:"none",
+          title:"请前往设置页面授权"
+        })
+      }
     })
   },
   onLoad() {
@@ -35,7 +95,7 @@ Page({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
-      }
+      },
     })
   },
   getUserInfo(e: any) {
